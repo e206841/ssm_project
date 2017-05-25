4fecd1255ca71c079b34fe082be006149fe77210
linan <e206841@163.com>
2017-05-25 17:18:07 +0800

    添加rabbitmq Demo

--- src/test/java/com/ln/web/controller/TestBase.java
+++ src/test/java/com/ln/web/controller/TestBase.java
@@ -7,7 +7,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)  
-@ContextConfiguration(locations = { "classpath*:applicationContext.xml"})
+@ContextConfiguration(locations = { "classpath*:applicationContext.xml",
+									"classpath*:spring-mvc.xml",
+									"classpath*:spring-mq.xml"})
 public abstract class TestBase extends AbstractJUnit4SpringContextTests{
 	
 
